53baadec525b9a3b9d120408797b0bba088c5d24
SEFYU92 <youssefelkorji92@gmail.com>
2015-11-06 18:49:14 +0100

    projet qualité logiciel

---
+++ src/View/Dashboard.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+/**
+ *
+ * @author Youssef
+ */
+public class Dashboard {
+    private boolean GreenLight;
+    private boolean RedLight;
+    private boolean OrangeLight;
+    public Dashboard()
+    {
+        GreenLight = false;
+        RedLight = false;
+        OrangeLight = false;
+    }
+    public void setGreenLight(boolean set)
+    {
+        GreenLight = set;
+    }
+    public void setRedLight(boolean set)
+    {
+        GreenLight = set;
+    }
+    public void setOrangeLight(boolean set)
+    {
+        GreenLight = set;
+    }
+}
